acf6062fb9f68594486b119fbb0c100a8efd4302
rickkas7 <rickk@rickk.com>
2021-04-04 08:33:12 -0400

    Documentation

--- src/SequentialFileRK.h
+++ src/SequentialFileRK.h
@@ -7,28 +7,100 @@
 
 /**
  * @brief Class for maintaining a directory of files as a queue with unique filenames
+ *
+ * You select a directory on the Gen 3 flash file system for use as the queue directory.
+ * This directory is filled with numbered files, for example: 00000001.jpg. It's 
+ * possible to store meta information in additional files. For example, 00000001.sha1
+ * to hold a SHA-1 hash of the file. The file are numbered sequentially.
  * 
+ * You typically instantiate this class as a global variable. 
  * 
+ * In global setup(), configure it using the withXXX() methods, then call the scanDir()
+ * method to read the queue from disk. In particular, you must use withDirPath() to set
+ * the queue directory!
+ * 
+ * Files left on disk are added to the queue during scanDir() and an in-memory queue
+ * is initialized with the files to process.
+ * 
+ * Code can reserve a space in the queue using reserveFile(). This returns the next
+ * available file number but does not create the file. 
+ * 
+ * Once you have fully written the file, call addFileToQueue().
+ * 
+ * The code that processes files in the queue calls getFileFromQueue(). 
  */
 class SequentialFile {
 public:
+    /**
+     * @brief Default constructor
+     * 
+     * This object is often created as a global variable. 
+     * 
+     * In global setup(), configure it using the withXXX() methods, then call the scanDir()
+     * method to read the queue from disk.
+     */
     SequentialFile();
+
+    /**
+     * @brief Destructor
+     * 
+     * This object is often created as a global variables and never deleted.
+     */
     virtual ~SequentialFile();
 
+    /**
+     * @brief Sets the directory to use as the queue directory. This is required!
+     * 
+     * @param dirPath the pathname, Unix-style with / as the directory separator. 
+     * 
+     * Typically you create your queue either at the top level ("/myqueue") or in /usr
+     * ("/usr/myqueue"). The directory will be created if necessary, however only one
+     * level of directory will be created. The parent must already exist.
+     * 
+     * The dirPath can end with a slash or not, but if you include it, it will be
+     * removed.
+     * 
+     * You must call this as you cannot use the root directory as a queue!
+     */
     SequentialFile &withDirPath(const char *dirPath);
 
+    /**
+     * @brief Gets the directory path set using withDirPath()
+     * 
+     * The returned path will not end with a slash.
+     */
     const char *getDirPath() const { return dirPath; };
 
+    /**
+     * @brief Sets the filename pattern. Default is %08d (8-digit decimal number with leading zeros)
+     * 
+     * @param pattern The sprintf/sscanf pattern to use
+     * 
+     * This string is used when scanning the queue directory to find files. Do not include the
+     * filename extension in this pattern!
+     */
     SequentialFile &withPattern(const char *pattern) { this->pattern = pattern; return *this; };
 
+    /**
+     * @brief Gets the filename pattern
+     */
     const char *getPattern() const { return pattern; };
     
     /**
      * @brief Filename extension for queue files. (Default: no extension)
      * 
-     * @param ext The filename extension. 
+     * @param ext The filename extension (just the extension, no preceding dot)
+     * 
+     * For example, for JPEG files it might be "jpg".
      */
     SequentialFile &withFilenameExtension(const char *ext) { this->filenameExtension = ext; return *this; };
+
+    /**
+     * @brief Gets the filename extension
+     * 
+     * Returned string does not contain the dot. For example, for JPEG files it might be "jpg".
+     */
+    const char *getFilenameExtension() const { return filenameExtension; };
 
     /**
      * @brief Scans the queue directory for files. Typically called during setup().
@@ -48,6 +120,9 @@ public:
      * 
      * Use reserveFile() to get the next file number, addFileToQueue() to add it to the queue
      * and getFileFromQueue() to get an item from the queue.
+     * 
+     * It's safe to call reserveFile(), addFileToQueue(), and getFileFromQueue() from different
+     * threads. Locking is handled internally.
      */
     void addFileToQueue(int fileNum);
 
@@ -59,7 +134,13 @@ public:
      * Use getPathForFileNum() to convert the number into a pathname for use with open().
      * 
      * The queue is stored in RAM, so if the device reboots before you delete the file it
-     * will reappear in the queue when scanDir is called.
+     * will reappear in the queue when scanDir is called. This method does not need to access 
+     * the filesystem. You can call getFileFromQueue() on every loop if you want to to check 
+     * if there are files to process without affecting performance.
+     * 
+     * It's safe to call reserveFile(), addFileToQueue(), and getFileFromQueue() from different
+     * threads. Locking is handled internally.
+     * 
      */
     int getFileFromQueue(void);
 
@@ -100,10 +181,17 @@ public:
     void removeFileNum(int fileNum, bool allExtensions);
 
     /**
-     * @brief
+     * @brief Removes all of the files in the queue directory
+     * 
+     * @brief removeDir true to remove the queue directory itself, false to just remove the contents.
+     * 
+     * Note this is all files, not just the filenames with the matching extension.
      */
     void removeAll(bool removeDir);
 
+    /**
+     * @brief Gets the length of the queue
+     */
     int getQueueLen() const;
 
     /**
@@ -138,10 +226,26 @@ public:
     static String getNameWithOptionalExt(const char *name, const char *ext);
 
 protected:
+    /**
+     * @brief Allows a subclass to choose whether to queue a file or not during scanDir.
+     * 
+     * @param name A potential filename in the queue directory
+     * 
+     * @return true to add the file to the queue or false to not queue the file.
+     * 
+     * This would allow a subclass to do some validation of the file before adding
+     * it to the queue when reading the files from disk after reboot.
+     */
     virtual bool preScanAddHook(const char *name) { return true; };
 
+    /**
+     * @brief Lock the mutex used to protect the queue
+     */
     void queueMutexLock() const;
 
+    /**
+     * @brief Unlock the mutex used to protect the queue
+     */
     void queueMutexUnlock() const;
 
 protected:
@@ -149,11 +253,40 @@ protected:
      * @brief The path to the queue directory. Must be configured, using the top level directory is not allowed
      */
     String dirPath;
+
+    /**
+     * @brief Filename pattern, used with snprintf and sscanf to parse and generate filenames.
+     */
     String pattern = "%08d";
+
+    /**
+     * @brief Filename extension, without the dot. May be an empty string for no extension.
+     */
     String filenameExtension = "";
+
+    /**
+     * @brief Set to true after scanDir() is called
+     */
     bool scanDirCompleted = false;
+
+    /**
+     * @brief Last file number used.
+     * 
+     * Set during scanDir() and incremented by reserveFile(). May be updated by addFileToQueue() if
+     * you didn't reserveFile() first.
+     */
     int lastFileNum = 0;
+
+    /**
+     * @brief Mutex used to protect queue
+     */
     mutable os_mutex_t queueMutex = 0;
+
+    /**
+     * @brief Queue of files
+     * 
+     * Items are added using scanDir() and addFileToQueue(). Removed using getFileFromQueue().
+     */
     std::deque<int> queue;
 };
 
